dce2b52fec0e54808a1e37d47875d2570e543c0e
MiguelGameDev <mlopez.gamedev@gmail.com>
2025-01-26 00:27:08 +0100

    feat: level 1

---
+++ Assets/#MiguelGameDev/#SnakeBattle/Scripts/Snake/Bubble.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace MiguelGameDev.SnakeBubble.Snake
+{
+    public class Bubble : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
